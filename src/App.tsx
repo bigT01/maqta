9c781be1ee06ef7c82e7af977e9323e97efcd5cd
tan <tanat8511@gmail.com>
2024-05-19 15:15:33 +0500

    feat: updated data

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ function App() {
                 <div className='flex flex-col gap-6'>
                     <div className='relative' onClick={() => setIsPopUp(true)} >
                         <img className={'rounded-t-2xl object-cover h-[144px] w-full'}
-                             src="https://images.unsplash.com/photo-1554118811-1e0d58224f24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D"
+                             src="https://avatars.mds.yandex.net/get-altay/1595534/2a0000016d6f67de6cfe8780c36d8e0d13bf/L_height"
                              alt="img:card"/>
                         <div className="absolute top-[120px] left-4">
                             <svg width="49" height="32" viewBox="0 0 49 32" fill="none"
@@ -77,7 +77,7 @@ function App() {
                                       fill="white"/>
                             </svg>
                             <img className={'absolute left-3 top-[4px] w-[24px] h-[24px] rounded-full'}
-                                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png"
+                                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN66FZrmXxT_u42Mebpd6SkXkOxLtTibbfKZdTBSJCKw&s"
                                  alt="img:logo"/>
                         </div>
                         <div className='bg-white h-[78px] rounded-b-2xl p-2'>
@@ -106,7 +106,7 @@ function App() {
                     </div>
                     <div className='relative' onClick={() => setIsPopUp(true)} >
                         <img className={'rounded-t-2xl object-cover h-[144px] w-full'}
-                             src="https://images.unsplash.com/photo-1554118811-1e0d58224f24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D"
+                             src="https://franchise-capital.com/wp-content/uploads/2024/01/franshyza-global-coffee.-franchise-global-coffee.franshyza-global-kofy-21.jpg"
                              alt="img:card"/>
                         <div className="absolute top-[120px] left-4">
                             <svg width="49" height="32" viewBox="0 0 49 32" fill="none"
@@ -120,36 +120,36 @@ function App() {
                                       fill="white"/>
                             </svg>
                             <img className={'absolute left-3 top-[4px] w-[24px] h-[24px] rounded-full'}
-                                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png"
+                                 src="https://franchise-capital.com/wp-content/uploads/2023/12/franshyza-global-coffee-franchajzyng-global-coffee-franchise-global-coffee.png"
                                  alt="img:logo"/>
                         </div>
                         <div className='bg-white h-[78px] rounded-b-2xl p-2'>
                             <div className="flex justify-between items-center mb-1">
-                                <h5 className='font-semibold text-base'>Tap tatti</h5>
-                                <h5 className='font-normal text-sm'>1500₸ ~ 3500₸</h5>
+                                <h5 className='font-semibold text-base'>Global Coffee</h5>
+                                <h5 className='font-normal text-sm'>2000₸ ~ 5000₸</h5>
                             </div>
                             <div className="flex justify-between items-center mb-1">
                                 <div className='flex gap-2 items-center'>
-                                    <p className='font-semibold text-xs'>4.5</p>
+                                    <p className='font-semibold text-xs'>4.0</p>
                                     <div className='flex gap-1'>
                                         <TiStarFullOutline color={'#F1AF65'} size={10}/>
                                         <TiStarFullOutline color={'#F1AF65'} size={10}/>
                                         <TiStarFullOutline color={'#F1AF65'} size={10}/>
-                                        <TiStarHalfOutline color={'#F1AF65'} size={10}/>
+                                        <TiStarFullOutline color={'#F1AF65'} size={10}/>
                                         <TiStarOutline color={'#F1AF65'} size={10}/>
                                     </div>
                                 </div>
                                 <p className='font-light text-xs'>в продаже с 16:00 до 18:00</p>
                             </div>
                             <div className="flex gap-2 items-center">
-                                <p className='font-normal text-xs text-[#ADADAD]'>(1.2 km)</p>
-                                <p className='font-normal text-xs'>Республика 34/1 </p>
+                                <p className='font-normal text-xs text-[#ADADAD]'>(2.2 km)</p>
+                                <p className='font-normal text-xs'>Иманова 5/1 </p>
                             </div>
                         </div>
                     </div>
                     <div className='relative' onClick={() => setIsPopUp(true)} >
                         <img className={'rounded-t-2xl object-cover h-[144px] w-full'}
-                             src="https://images.unsplash.com/photo-1554118811-1e0d58224f24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D"
+                             src="https://avatars.mds.yandex.net/get-altay/1871297/2a00000185b72892f7ff13422820c1d6dc83/orig"
                              alt="img:card"/>
                         <div className="absolute top-[120px] left-4">
                             <svg width="49" height="32" viewBox="0 0 49 32" fill="none"
@@ -163,30 +163,30 @@ function App() {
                                       fill="white"/>
                             </svg>
                             <img className={'absolute left-3 top-[4px] w-[24px] h-[24px] rounded-full'}
-                                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1024px-LEGO_logo.svg.png"
+                                 src="https://images.satu.kz/114029139_w640_h640_114029139.jpg"
                                  alt="img:logo"/>
                         </div>
                         <div className='bg-white h-[78px] rounded-b-2xl p-2'>
                             <div className="flex justify-between items-center mb-1">
-                                <h5 className='font-semibold text-base'>Tap tatti</h5>
-                                <h5 className='font-normal text-sm'>1500₸ ~ 3500₸</h5>
+                                <h5 className='font-semibold text-base'>Zebra Coffee</h5>
+                                <h5 className='font-normal text-sm'>1000₸ ~ 4000₸</h5>
                             </div>
                             <div className="flex justify-between items-center mb-1">
                                 <div className='flex gap-2 items-center'>
-                                    <p className='font-semibold text-xs'>4.5</p>
+                                    <p className='font-semibold text-xs'>3.5</p>
                                     <div className='flex gap-1'>
                                         <TiStarFullOutline color={'#F1AF65'} size={10}/>
                                         <TiStarFullOutline color={'#F1AF65'} size={10}/>
-                                        <TiStarFullOutline color={'#F1AF65'} size={10}/>
                                         <TiStarHalfOutline color={'#F1AF65'} size={10}/>
+                                        <TiStarOutline color={'#F1AF65'} size={10}/>
                                         <TiStarOutline color={'#F1AF65'} size={10}/>
                                     </div>
                                 </div>
                                 <p className='font-light text-xs'>в продаже с 16:00 до 18:00</p>
                             </div>
                             <div className="flex gap-2 items-center">
-                                <p className='font-normal text-xs text-[#ADADAD]'>(1.2 km)</p>
-                                <p className='font-normal text-xs'>Республика 34/1 </p>
+                                <p className='font-normal text-xs text-[#ADADAD]'>(4.1 km)</p>
+                                <p className='font-normal text-xs'>Женис 10/6 </p>
                             </div>
                         </div>
                     </div>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,12 @@
 import {Link, NavLink, Outlet} from "react-router-dom";
-import {AiOutlineCloseCircle, AiOutlineHeart, AiOutlineHome, AiOutlineShopping, AiOutlineUser} from "react-icons/ai";
+import {
+    AiFillHeart,
+    AiOutlineCloseCircle,
+    AiOutlineHeart,
+    AiOutlineHome,
+    AiOutlineShopping,
+    AiOutlineUser
+} from "react-icons/ai";
 import {useStore} from "../store/useStore.ts";
 import {useEffect, useState} from "react";
 import {GrTarget} from "react-icons/gr";
@@ -10,19 +17,20 @@ const Layout = () => {
     const isPopUp = useStore(state => state.isPopUp)
     const [isAnimation, setIsAnimation] = useState(false)
     const [isClose, setIsClose] = useState(false)
-    const setIsPopUp = useStore(state => state.setIsPopUp)
+    const setIsPopUp = useStore(state => state.setIsPopUp);
+    const [liked, setLiked] = useState(false)
 
     useEffect(() => {
-        if(isPopUp){
+        if (isPopUp) {
             document.body.style.overflowY = 'hidden';
-        } else{
+        } else {
             document.body.style.overflowY = 'auto';
         }
 
     }, [isPopUp])
 
     useEffect(() => {
-        if(isClose){
+        if (isClose) {
             setTimeout(() => {
                 setIsPopUp(false)
                 setIsAnimation(false)
@@ -35,30 +43,32 @@ const Layout = () => {
         <div className={'w-screen  min-h-screen bg-[#EFF3D6] relative'}>
             <Outlet/>
 
-            <div className={'w-full h-[60px] justify-between bg-white fixed bottom-0 rounded-t-3xl flex items-center px-[30px]'}>
-                <NavLink to={'/'} >
-                    <AiOutlineHome size={32} />
+            <div
+                className={'w-full h-[60px] justify-between bg-white fixed bottom-0 rounded-t-3xl flex items-center px-[30px]'}>
+                <NavLink to={'/'}>
+                    <AiOutlineHome size={32}/>
                 </NavLink>
-                <NavLink to={'/'} >
-                    <AiOutlineShopping size={32} />
+                <NavLink to={'/'}>
+                    <AiOutlineShopping size={32}/>
                 </NavLink>
-                <NavLink to={'/'} >
-                    <AiOutlineHeart size={32} />
+                <NavLink to={'/'}>
+                    <AiOutlineHeart size={32}/>
                 </NavLink>
-                <NavLink to={'/'} >
+                <NavLink to={'/'}>
                     <AiOutlineUser size={32}/>
                 </NavLink>
 
             </div>
 
-            {isPopUp && <div className={`fixed z-50 bg-white top-0 left-0 w-full h-screen overflow-y-hidden ${isAnimation ? 'fade-in-bottom' :'fade-in-top'}`}>
+            {isPopUp && <div
+                className={`fixed z-50 bg-white top-0 left-0 w-full h-screen overflow-y-hidden ${isAnimation ? 'fade-in-bottom' : 'fade-in-top'}`}>
                 <div className='relative w-full h-full'>
                     <button className={'absolute right-5 top-5 bg-[#ADADAD] rounded-full'} onClick={() => {
                         setIsClose(true)
                         setIsAnimation(true)
                     }}><AiOutlineCloseCircle size={32}/></button>
                     <img className='w-full h-[322px] object-cover'
-                         src="https://neurosciencenews.com/files/2023/06/coffee-brain-caffeine-neuroscincces.jpg"
+                         src="https://restoran.kz/neofiles/serve-image/63c11364dd1c0d247f0e690c"
                          alt="img:popUp"/>
                     <div className={'h-[70vh] absolute bottom-0 w-full bg-[#E5EBBE] rounded-t-3xl '}>
                         <div className='flex justify-between py-4 w-[90%] mx-auto'>
@@ -70,8 +80,8 @@ const Layout = () => {
                                 <LuDessert size={32}/>
                                 <p className='font-normal text-base'>100+ десертов</p>
                             </div>
-                            <div className='flex-col flex items-center gap-1'>
-                                <AiOutlineHeart size={32}/>
+                            <div className='flex-col flex items-center gap-1' onClick={() => setLiked((old) => !old)}>
+                                <AiFillHeart size={32} color={liked ? 'red' : '#919191'}/>
                                 <p className='font-normal text-base'>90+ отзывов</p>
                             </div>
                         </div>
@@ -102,18 +112,22 @@ const Layout = () => {
                                 </div>
                                 <div className='gap-4 flex flex-col'>
                                     <h5 className='text-lg font-semibold'>Описание:</h5>
-                                    <p className='text-base '>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <p className='text-base '>Тап Татти - это уютная кондитерская в Казахстане,
+                                        предлагающая своим гостям восхитительные десерты и выпечку, приготовленные с
+                                        любовью и вниманием к деталям. В нашем меню вы найдете разнообразные торты,
+                                        пирожные, печенье и другие сладости, которые станут идеальным дополнением к
+                                        вашему утреннему кофе или вечернему чаю. Мы используем только высококачественные
+                                        ингредиенты, чтобы наши десерты радовали вас не только вкусом, но и пользой. Тап
+                                        Татти - это место, где каждый найдет что-то по своему вкусу и сможет насладиться
+                                        атмосферой уюта и тепла.</p>
                                 </div>
                             </div>
                         </div>
-                        <div className='absolute flex items-center justify-center bottom-5 w-[90%] z-50 left-[50%] bg-[#C7E60C20] rounded-full h-[54px]'
-                             style={{transform: 'translateX(-50%)'}} >
-                            <Link to={'https://wa.me/77002132703'} className={'bg-[#C7E60C] rounded-full py-2 w-[90%] flex justify-center'}>Заказать</Link>
+                        <div
+                            className='absolute flex items-center justify-center bottom-5 w-[90%] z-50 left-[50%] bg-[#C7E60C20] rounded-full h-[54px]'
+                            style={{transform: 'translateX(-50%)'}}>
+                            <Link to={'https://wa.me/77002132703'}
+                                  className={'bg-[#C7E60C] rounded-full py-2 w-[90%] flex justify-center'}>Заказать</Link>
                         </div>
                     </div>
                 </div>
